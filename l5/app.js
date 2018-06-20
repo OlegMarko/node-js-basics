99859a364330826126bc56e446b972679c6d3464
Oleg Marko <olegmarko7@gmail.com>
2018-06-20 09:08:39 +0300

    Form validation

--- l5/app.js
+++ l5/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressValidator = require('express-validator');
+var expressSession = require('express-session');
 
 var indexRouter = require('./routes/index');
 
@@ -15,8 +17,14 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressSession({
+    secret: 'max',
+    saveUninitialized: false,
+    resave: false
+}));
 
 app.use('/', indexRouter);
 
--- l5/routes/index.js
+++ l5/routes/index.js
@@ -2,8 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', condition: true });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Express',
+        condition: true,
+        success: req.session.success,
+        errors: req.session.errors
+    });
+    req.session.errors = null;
+});
+
+router.post('/submit', function (req, res, next) {
+    // Check validity
+    req.check('email', 'Invalid email address').isEmail();
+    req.check('password', 'Invalid password').isLength({min: 4});
+    req.check('confirmPassword', 'Confirmation password not match').equals(req.body.password);
+
+    let errors = req.validationErrors();
+    if (errors) {
+        req.session.errors = errors;
+        req.session.success = false;
+    } else {
+        req.session.success = true;
+    }
+
+    res.redirect('/');
 });
 
 /* GET users listing. */
